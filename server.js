b876f85306a84da2edd1903e7919a063c0481cee
Prerit2002 <Prerit715@gmail.com>
2021-10-02 22:22:52 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect( "mongodb+srv://admin:BM81WnYHfJGKm2RU@cluster0.wdu17.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ,
+    {
+      useNewUrlParser: true, // we do this to avoid the depracation warning
+      useUnifiedTopology: true, //The useUnifiedTopology option removes support for several connection options that are no longer relevant with the new engine:
+      
+    }
+  )
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+
+app.listen(4000, () => {
+  console.log("listening on port 4000");
+});
