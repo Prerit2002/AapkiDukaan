ab81b71085f8bb3947234f7b306dd196581d4244
mohit040 <mohit.chourasia2020@vitstudent.ac.in>
2021-10-02 22:47:15 +0530

    first commit

---
+++ model/seller.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose')
+
+
+const personalDetails = new mongoose.Schema({
+    sellerName:{
+        type:String,
+        required:true
+    },
+    Address : {
+        type:String,
+        required:true
+    },
+    shopName:{
+        type:String,
+        required:true
+    },
+    domain: {
+        type:String,
+        required:true
+    },
+    gender: {
+        type:String,
+    }
+})
+
+const Billing = new Mongoose.Schema({
+   bill: [{
+       billAmount: {
+        type:Number,
+        required:true
+       },
+       startDate: {
+           type: Date,
+           required:true
+       },
+       endDate: {
+        type:Date,
+        required:true
+       },
+       paymentStatus: {
+        type:Boolean,
+        required:true
+       },
+       dueDate:{
+        type:Date,
+        required:true
+       }
+   }]
+})
+
+const sellerSchema = new Mongoose.Schema({
+    personalDetails = [personalDetails],
+    bills: Billing,
+
+
+
+})
+
+
--- server.js
+++ server.js
@@ -1,26 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
-
 const app = express();
-
-app.use(express.json());
-
-mongoose
-  .connect( "mongodb+srv://admin:BM81WnYHfJGKm2RU@cluster0.wdu17.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ,
-    {
-      useNewUrlParser: true, // we do this to avoid the depracation warning
-      useUnifiedTopology: true, //The useUnifiedTopology option removes support for several connection options that are no longer relevant with the new engine:
-      
-    }
-  )
-  .then(() => {
-    console.log("db connected");
-  })
-  .catch((e) => {
-    console.log(e);
-  });
+const connectDB = require('./server/database/connection');
 
 
-app.listen(4000, () => {
-  console.log("listening on port 4000");
+connectDB();
+
+app.use(express.urlencoded({ extended : true}))
+
+
+app.listen(5000, () => {
+  console.log("listening on port 5000");
 });
